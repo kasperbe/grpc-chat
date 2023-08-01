002645279e80e4046b8559ce19bb5ddfb2a6a728
Kasper Bentsen <kas.bentsen@gmail.com>
2023-08-01 15:03:31 +0200

    simple pub/sub chat server

--- server/main.go
+++ server/main.go
@@ -12,18 +12,62 @@ import (
 	"google.golang.org/grpc/reflection"
 )
 
-type server struct {
-	pb.UnimplementedChatServer
+func NewServer() *Server {
+	return &Server{
+		Connections: map[string]chan *pb.ChatMessage{},
+	}
 }
 
-func (s *server) Send(ctx context.Context, in *pb.ChatMessage) (*pb.ChatResponse, error) {
+type Server struct {
+	pb.UnimplementedChatServer
+	Connections map[string]chan *pb.ChatMessage
+}
+
+func (s *Server) Send(ctx context.Context, in *pb.ChatMessage) (*pb.ChatResponse, error) {
+	msg := &pb.ChatMessage{
+		UserId:    in.UserId,
+		Message:   in.Message,
+		MessageId: "123",
+	}
+
+	if c, ok := s.Connections[in.UserId]; ok {
+		c <- msg
+
+		return &pb.ChatResponse{
+			Status:  200,
+			Message: "message_sent",
+		}, nil
+	}
+
+	c := make(chan *pb.ChatMessage, 15)
+	s.Connections[in.UserId] = c
+	c <- msg
+
 	return &pb.ChatResponse{
 		Status:  200,
-		Message: "test",
+		Message: "message_sent",
 	}, nil
 }
 
-func (s *server) Listen(in *pb.Subscribe, stream pb.Chat_ListenServer) error {
+func (s *Server) Listen(in *pb.Subscribe, stream pb.Chat_ListenServer) error {
+
+	var ch chan *pb.ChatMessage
+
+	if c, ok := s.Connections[in.UserId]; ok {
+		ch = c
+	} else {
+		ch = make(chan *pb.ChatMessage, 15)
+		s.Connections[in.UserId] = ch // Guard this with some mutex
+	}
+
+	for msg := range ch {
+		stream.Send(&pb.ChatMessage{
+			MessageId: msg.MessageId,
+			Message:   msg.Message,
+			UserId:    msg.UserId,
+		})
+	}
+
 	for {
 		time.Sleep(1 * time.Second)
 
@@ -41,7 +85,7 @@ func main() {
 	}
 
 	s := grpc.NewServer()
-	pb.RegisterChatServer(s, &server{})
+	pb.RegisterChatServer(s, NewServer())
 	reflection.Register(s)
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("serve: %v", err)
